a8669d0abbbdfa518a0c2129fcda5815da46f0d9
NandiniArjun <30292911+NandiniArjun@users.noreply.github.com>
2019-06-27 18:55:35 +0100

    Add files via upload

--- src/main/java/com/bpdts/rest/controller/UserController.java
+++ src/main/java/com/bpdts/rest/controller/UserController.java
@@ -1,1 +1,28 @@
+package com.bpdts.rest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+ 
+import com.bpdts.rest.dao.UserDAO;
+import com.bpdts.rest.model.Users;
+
+@RestController
+public class UserController {
+	
+	@Autowired
+    private UserDAO userDao;
+     
+    @GetMapping(path="/city/{city}/users", produces = "application/json")
+    public Users getUsersByCity(@PathVariable String city) 
+    {
+        return userDao.getUsersByCity(city);
+    }
+    
+    @GetMapping(path="/users", produces = "application/json")
+    public Users getUsersByLatLon() 
+    {
+        return userDao.getUsersByLatLon();
+    }
+}
