94fe8aff36f410a875c26d6d31251f96ba839bb0
NandiniArjun <30292911+NandiniArjun@users.noreply.github.com>
2019-06-27 18:54:09 +0100

    Add files via upload

--- src/main/java/com/bpdts/rest/FetchUsers.java
+++ src/main/java/com/bpdts/rest/FetchUsers.java
@@ -1,1 +1,13 @@
+package com.bpdts.rest;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class FetchUsers {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchUsers.class, args);
+	}
+
+}
